4ddfbfaf2b71518db92628de89197a09f65e8c7e
Emmanuel-spring <e.nieto@alumnos.santotomas.cl>
2021-10-09 18:16:22 -0300

    Deitel y Deitel 01

---
+++ Deitel&&Deitel/src/Capitulo06/EnterosAleatorios.java
@@ -0,0 +1,29 @@
+package Capitulo06;
+
+import java.util.Random;
+
+/**  Fig 6.7: EnterosAleatorios.java
+ *  Enteros aleatorios desplazados y escalados
+ *
+ * @version 09/10/2021
+ * @author Emmanuel Nieto Muñoz
+ * @see
+ */
+
+public class EnterosAleatorios {
+    public static void main(String[] args) {
+
+        Random numerosAleatorios = new Random();  // Generador de números enteros aleatorios
+        int cara;  // Almacena cada entero aleatorio generado
+
+        for (int contador = 1; contador <= 20; contador++) {
+            cara = 1 + numerosAleatorios.nextInt(6);
+
+            System.out.printf("%d ", cara);  // Muestra el valor generado
+
+            // Si contador es divisible entre 5, empieza una nueva línea de salida
+            if (contador % 5 == 0)
+                System.out.println();
+        } // fin de for
+    }  // fin de main
+} // fin de la clase EnterosAleatorios
